0686c5deef4f856614274c8b5f437ad8ccba7419
Tim Retout <tim@retout.co.uk>
2026-06-01 23:18:41 +0100

    fix: Choose smc or hvc depending on hypervisor support

--- loader/src/aarch64/smc.h
+++ loader/src/aarch64/smc.h
@@ -11,6 +11,12 @@
 #include "../cutil.h"
 #include "../uart.h"
 
+#ifdef CONFIG_ARM_HYPERVISOR_SUPPORT
+#define ARM_SMCCC_CONDUIT "smc #0\n"
+#else
+#define ARM_SMCCC_CONDUIT "hvc #0\n"
+#endif
+
 /**
  * The Power State Coordinate Interface (DEN0022F.b) document in §5.2.1
  * specifies that
@@ -96,7 +102,7 @@ static inline uint64_t arm_smc64_call(uint32_t function_id, uint64_t arg0, uint6
     register uint64_t x2 asm("x2") = arg1;
     register uint64_t x3 asm("x3") = arg2;
     asm volatile(
-        "smc #0\n"
+        ARM_SMCCC_CONDUIT
         : "=r"(x0)
         : "r"(x0), "r"(x1), "r"(x2), "r"(x3)
         : "x4", "x5", "x6", "x7", "x8", "x9", "x10", "x11", "x12", "x13", "x14", "x15", "x16", "x17",
@@ -124,7 +130,7 @@ static inline uint32_t arm_smc32_call(uint32_t function_id, uint32_t arg0, uint3
     register uint64_t w2 asm("w2") = arg1;
     register uint64_t w3 asm("w3") = arg2;
     asm volatile(
-        "smc #0\n"
+        ARM_SMCCC_CONDUIT
         : "=r"(w0)
         : "r"(w0), "r"(w1), "r"(w2), "r"(w3)
         : "w4", "w5", "w6", "w7", "w8", "w9", "w10", "w11", "w12", "w13", "w14", "w15", "w16", "w17",
